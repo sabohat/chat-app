974e853b99329c27b9c008ab095d1b27b031c8f7
sabohat <sabohat2802@gmail.com>
2021-05-03 13:35:54 +0500

    fixed mongo string

--- modules/mongo.js
+++ modules/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-
+const CONNECTION_STRING = process.env.CONNECTION_STRING
 async function client () {
-    let server = await mongoose.connect(`mongodb://localhost/chat-app`, {
+    let server = await mongoose.connect(CONNECTION_STRING, {
         useCreateIndex: true,
         useFindAndModify: false,
         useNewUrlParser: true,
